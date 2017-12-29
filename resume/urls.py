248a770a08a66f9ff35ab0aec7e5555135ca8e91
장지용 <jangjiyong@jangjiyong-ui-MacBook-Pro.local>
2017-12-29 21:15:04 +0900

    이력서 생성 및 인덱스 페이지와 연결

--- resume/urls.py
+++ resume/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path,re_path,include
+from django.views.generic.base import TemplateView
 
 from . import views
 
 app_name = "resume"
 
 urlpatterns = [
-    re_path('^$',views.Index.as_view(), name="index"),
+    re_path(r'^$',views.Index.as_view(), name="index"),
+    re_path(r'^resume/$', TemplateView.as_view(template_name="resume/resume.html"), name="resume"),
 ]
